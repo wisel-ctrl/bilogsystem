0a2a4fec7caa8531661441021d82b986f3897422
law-study <lawrencerebong09@gmail.com>
2025-06-05 14:55:34 +0800

    Update admin_menu.php

--- admin/admin_menu.php
+++ admin/admin_menu.php
@@ -25,8 +25,12 @@
     <title>Cafe Lilio - Menu Management</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
-    <style>
+    <!-- Add these to your head section -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
         .chart-container {
             position: relative;
             height: 300px;
