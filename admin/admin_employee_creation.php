49727d8fc365744ac390c2310aab725fa82eaf30
jimdmnc <rylsnchz@gmail.com>
2025-06-25 10:09:29 +0800

    sdsd

--- admin/admin_employee_creation.php
+++ admin/admin_employee_creation.php
@@ -774,7 +774,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['form_type']) && $_POS
                                         <button class="edit-btn text-rich-brown hover:text-deep-brown transition-colors duration-200 mr-2" data-id="<?php echo htmlspecialchars($cashier['id']); ?>">
                                             <i class="fas fa-edit text-lg"></i>
                                         </button>
-                                        <button class="archive-btn text-blue-500 hover:text-red-700 transition-colors duration-200" data-id="<?php echo htmlspecialchars($cashier['id']); ?>">
+                                        <button class="archive-btn text-blue-800 hover:text-red-700 transition-colors duration-200" data-id="<?php echo htmlspecialchars($cashier['id']); ?>">
                                             <i class="fas fa-archive mr-2 text-lg"></i>
                                         </button>
                                     </td>
