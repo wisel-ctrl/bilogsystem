8741b2758b977dd95cfcd2fbb6450efb8ec091c7
jimdmnc <rylsnchz@gmail.com>
2025-06-27 06:10:55 +0800

    sdsdd

--- admin/admin_expenses.php
+++ admin/admin_expenses.php
@@ -334,7 +334,7 @@
             </div>
             
             <!-- Modal content -->
-            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full z-[100]">
                 <div class="modal-header bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <div class="sm:flex sm:items-start">
                         <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
