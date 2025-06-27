0c9ea4803104c74769b4334a9969ac8b9dac260b
jimdmnc <rylsnchz@gmail.com>
2025-06-27 09:13:41 +0800

    sdsds

--- admin/admin_expenses.php
+++ admin/admin_expenses.php
@@ -69,7 +69,7 @@ ob_start();
 </div>
 
 <!-- Modal for editing expenses -->
-<div id="edit-expense-modal" class="fixed inset-0 z-50 hidden overflow-y-auto">
+<div id="edit-expense-modal" class="fixed inset-0 z-[100] hidden overflow-y-auto">
     <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <!-- Background overlay -->
         <div class="fixed inset-0 transition-opacity" aria-hidden="true">
