e95fe5d4959c22806d2d3ac46e91bbbbdc6ce92b
law-study <lawrencerebong09@gmail.com>
2025-07-02 20:34:14 +0800

    Update admin_bookings.php

--- admin/admin_bookings.php
+++ admin/admin_bookings.php
@@ -980,6 +980,8 @@
         return str.replace(/'/g, "\\'");
     }
     </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js"></script>
 <?php
 $page_scripts = ob_get_clean();
 
