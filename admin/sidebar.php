3994b858e50ee517a2632ee29ff05763ff9e6347
jimdmnc <rylsnchz@gmail.com>
2025-07-04 07:47:03 +0800

    sdsdsdsd

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -56,7 +56,7 @@
                 </a>
             </li>
             <li>
-                <a href="admin_reports.php" class="sidebar-link flex items-center p-3 rounded-lg hover:bg-warm-cream/20 text-warm-cream/80 hover:text-warm-cream transition-all duration-200 <?php echo basename($_SERVER['PHP_SELF']) === 'admin_employee_creation.php' ? 'active' : ''; ?>">
+                <a href="admin_reports.php" class="sidebar-link flex items-center p-3 rounded-lg hover:bg-warm-cream/20 text-warm-cream/80 hover:text-warm-cream transition-all duration-200 <?php echo basename($_SERVER['PHP_SELF']) === 'admin_reports.php' ? 'active' : ''; ?>">
                     <i class="fas fa-user-plus w-5"></i>
                     <span class="sidebar-text font-baskerville">Reports</span>
                     <span class="tooltip">Reports</span>
