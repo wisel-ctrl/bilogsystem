7ae855eab2b2578a9b4b6fb5b2495f8a135cfa81
jimdmnc <rylsnchz@gmail.com>
2025-07-06 08:14:48 +0800

    sdsdds

--- logout.php
+++ logout.php
@@ -1,19 +1,46 @@
 <?php
+// Determine which session to destroy based on the 'usertype' parameter
+$usertype = $_GET['usertype'] ?? '';
+
+$session_name = '';
+switch ($usertype) {
+    case 'admin':
+        $session_name = 'ADMIN_SESSION';
+        break;
+    case 'cashier':
+        $session_name = 'CASHIER_SESSION';
+        break;
+    case 'customer':
+        $session_name = 'CUSTOMER_SESSION';
+        break;
+    default:
+        // Invalid usertype, redirect to login
+        header("Location: login.php");
+        exit();
+}
+
+// Set the session name
+session_name($session_name);
 session_start();
 
 // Unset all session variables
 $_SESSION = array();
 
-// If it's desired to kill the session, also delete the session cookie
+// Delete the session cookie
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
+    setcookie(
+        $session_name,
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
     );
 }
 
-// Finally, destroy the session
+// Destroy the session
 session_destroy();
 
 // Redirect to login page
