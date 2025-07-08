e99912f440c70d85c4dd0927db3c048995a4dbee
jimdmnc <rylsnchz@gmail.com>
2025-07-09 07:13:10 +0800

    sdsds

--- index.php
+++ index.php
@@ -301,7 +301,7 @@
 
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 <!-- Best-Sellers -->
-                <a href="index_menu/best-sellers.php" class="relative rounded-xl overflow-hidden shadow-lg group cursor-pointer">
+                <a href="index_menu/best_sellers.php" class="relative rounded-xl overflow-hidden shadow-lg group cursor-pointer">
                     <div class="aspect-[3/4] relative">
                         <img src="images/cheesewheelpasta.jpg" alt="Best Sellers" class="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300">
                         <div class="absolute inset-0 bg-black bg-opacity-40 group-hover:bg-opacity-60 transition-all duration-300 flex items-center justify-center">
