2fff46dc5f2053af883231336652b163c3be606c
jimdmnc <rylsnchz@gmail.com>
2025-07-09 07:47:54 +0800

    dssdsd

--- index.php
+++ index.php
@@ -364,7 +364,7 @@
                 <!-- Pizza & Burgers -->
                 <a href="index_menu/burger_pizza.php" class="relative rounded-xl overflow-hidden shadow-lg group cursor-pointer">
                     <div class="aspect-[3/4] relative">
-                        <img src="images/spanish-dish.jpg" alt="Spanish Dish" class="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300">
+                        <img src="images/Pizza_Milano.jpg" alt="Spanish Dish" class="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300">
                         <div class="absolute inset-0 bg-black bg-opacity-40 group-hover:bg-opacity-60 transition-all duration-300 flex items-center justify-center">
                             <h3 class="font-playfair text-2xl font-bold text-warm-cream text-center px-4">Burgers & Pizza</h3>
                         </div>
@@ -374,7 +374,7 @@
                 <!-- Pasta -->
                 <a href="index_menu/pasta.php" class="relative rounded-xl overflow-hidden shadow-lg group cursor-pointer">
                     <div class="aspect-[3/4] relative">
-                        <img src="images/burgers-pizza.jpg" alt="Burgers & Pizza" class="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300">
+                        <img src="images/Carbonara.jpg" alt="Burgers & Pizza" class="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300">
                         <div class="absolute inset-0 bg-black bg-opacity-40 group-hover:bg-opacity-60 transition-all duration-300 flex items-center justify-center">
                             <h3 class="font-playfair text-2xl font-bold text-warm-cream text-center px-4">Pasta</h3>
                         </div>
@@ -384,7 +384,7 @@
                 <!-- Pasta e Caza -->
                 <a href="index_menu/pasta_ecaza.php" class="relative rounded-xl overflow-hidden shadow-lg group cursor-pointer">
                     <div class="aspect-[3/4] relative">
-                        <img src="images/pasta-salads.jpg" alt="Pasta & Salads" class="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300">
+                        <img src="images/pastaEcaza.png" alt="Pasta & Salads" class="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300">
                         <div class="absolute inset-0 bg-black bg-opacity-40 group-hover:bg-opacity-60 transition-all duration-300 flex items-center justify-center">
                             <h3 class="font-playfair text-2xl font-bold text-warm-cream text-center px-4">Pasta e Caza</h3>
                         </div>
