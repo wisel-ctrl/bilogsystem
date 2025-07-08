16b1f0ff91eebe83d4b31f8d18b61c872aca393f
jimdmnc <rylsnchz@gmail.com>
2025-07-08 08:35:23 +0800

    sdsdsdd

--- index_menu/italian_dish.php
+++ index_menu/italian_dish.php
@@ -6,6 +6,7 @@
     <title>Italian Dish Menu - Caffè Lilio</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <script src="tailwind.js"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap');
         
