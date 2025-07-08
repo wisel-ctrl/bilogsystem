778855ed97272145d28933d410b1267fe9a1a502
jimdmnc <rylsnchz@gmail.com>
2025-07-08 08:46:45 +0800

    sdsdd

--- customer/layout_customer.php
+++ customer/layout_customer.php
@@ -19,8 +19,8 @@ require_once '../db_connect.php';
     <script src="https://unpkg.com/tippy.js@6"></script>
 
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    
     <title>Menu - Caffè Lilio</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
