212bf66ab6e5384fc33f5f1dabad8618fe7e7916
jimdmnc <rylsnchz@gmail.com>
2025-07-14 15:25:04 +0800

    sdsd

--- customer/bookingAPI/get_package_dishes.php
+++ customer/bookingAPI/get_package_dishes.php
@@ -10,30 +10,27 @@ $packageId = $_GET['package_id'];
 
 try {
     $stmt = $conn->prepare("
-        SELECT 
-            mp.package_id,
-            mp.package_name,
-            mp.package_description,
-            mp.price,
-            mpd.dish_id,
-            mpd.quantity,
-            d.dish_name,
-            d.dish_category,
-            CASE WHEN bs.status = 'show' THEN 1 ELSE 0 END AS is_best_seller
-        FROM 
-            menu_packages_tb AS mp
-        JOIN 
-            menu_package_dishes_tb AS mpd ON mp.package_id = mpd.package_id
-        JOIN 
-            dishes_tb AS d ON mpd.dish_id = d.dish_id
-        LEFT JOIN 
-            best_seller_tb AS bs ON d.dish_id = bs.dish_id AND bs.status = 'show' AND bs.deleted_at IS NULL
-        WHERE 
-            mp.package_id = ?
-        ORDER BY 
-            FIELD(d.dish_category, 'house_salad', 'spanish_dish', 'italian_dish', 'burgers_pizza', 'pasta', 'pasta_caza', 'main_course', 'drinks', 'coffee', 'desserts'),
-            d.dish_name
-    ");
+    SELECT 
+        mp.package_id,
+        mp.package_name,
+        mp.package_description,
+        mp.price,
+        mpd.dish_id,
+        mpd.quantity,
+        d.dish_name,
+        d.dish_category
+    FROM 
+        menu_packages_tb AS mp
+    JOIN 
+        menu_package_dishes_tb AS mpd ON mp.package_id = mpd.package_id
+    JOIN 
+        dishes_tb AS d ON mpd.dish_id = d.dish_id
+    WHERE 
+        mp.package_id = ?
+        AND d.dish_category = 'best_seller'
+    ORDER BY 
+        d.dish_name
+");
     
     $stmt->execute([$packageId]);
     $dishes = $stmt->fetchAll(PDO::FETCH_ASSOC);
