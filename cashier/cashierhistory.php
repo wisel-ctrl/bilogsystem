40bcac3e9b04e8e07cc1f54c6bf7e617430ec653
law-study <lawrencerebong09@gmail.com>
2025-07-18 14:47:11 +0800

    Update cashierhistory.php

--- cashier/cashierhistory.php
+++ cashier/cashierhistory.php
@@ -1,6 +1,6 @@
 <?php 
 require_once 'cashier_auth.php'; 
-
+$userId = $_SESSION['user_id'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -184,6 +184,10 @@ require_once 'cashier_auth.php';
     </main>
 
     <script>
+        <script>
+            const userId = <?php echo json_encode($userId); ?>;
+            console.log("Logged in User ID:", userId);
+        </script>
         // Set current date
         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         const currentDate = new Date().toLocaleDateString('en-US', options);
